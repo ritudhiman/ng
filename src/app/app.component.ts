c903b910513b6fe5857a0489939630b901330417
Ritu Dhiman <ritu.dhiman97@gmail.com>
2018-05-31 18:34:18 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+ 
+@Component ({
+   selector: 'my-app',
+   templateUrl: 'app/app.component.html'
+})
+
+export class AppComponent {
+   appTitle: string = 'Welcome';
+   appList: any[] = [ {
+      "ID": "1",
+      "Name" : "One"
+   },
+
+   {
+      "ID": "2",
+      "Name" : "Two"
+   } ];
+}
